4d62ea3614942125e64f24e45aeb21c4fe34c3be
GETH <chamelehle@mail.csuchico.edu>
2016-10-31 16:41:46 -0700

    commit

--- sales.js
+++ sales.js
@@ -15,7 +15,7 @@ function addItem()
     var dollars = asCurrency(runningTotal);
     document.getElementById("subtotal").innerHTML = dollars;
     document.getElementById("price").innerHTML = "";
-    setCookie(preTax, runningTotal, 3);
+    setCookie("preTax", runningTotal, 3);
 
   }
   //IF newItem is not a number
