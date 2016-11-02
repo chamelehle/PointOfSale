ff9d324c86c83ec30caa7bc118a647746841297b
GETH <chamelehle@mail.csuchico.edu>
2016-11-02 16:02:13 -0700

    commit

--- sales.js
+++ sales.js
@@ -19,15 +19,7 @@ function addItem()
     setCookie("preTax", runningTotal, 3);
 
   }
-  function calculateReceipt()
-  {
-    var receiptSubtotal = Number(getCookie("preTax"));
-    var receiptTax = receiptSubtotal * 0.075;
-    var receiptTotal = receiptSubtotal + receiptTax;
-    document.getElementById("tax").innerHTML = asCurrency(receiptTax);
-    document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
-    document.getElementById("total").innerHTML = asCurrency(receiptTotal);
-  }
+
   //IF newItem is not a number
   // THEN show an alert: "Enter price as a number"
   //OTHERWISE,
@@ -39,14 +31,23 @@ function addItem()
   // update the value of the input with the id "price" to be an empty string
   // update a cookie called "preTax" with the value of runningTotal
 }
+function calculateReceipt()
+  {
+    var receiptSubtotal = Number(getCookie("preTax"));
+    var receiptTax = receiptSubtotal * 0.075;
+    var receiptTotal = receiptSubtotal + receiptTax;
+    document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+    document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+    document.getElementById("tot").innerHTML = asCurrency(receiptTotal);
+  }
 
 //takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
   return "$" + val.toFixed(2);
 }
-
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
